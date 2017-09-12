9702eaa4c00fa13e0534b4fb5b5e3d7978219830
Wisnu Sanjaya <wisnu@sebangsa.com>
2017-09-12 17:21:49 +0700

    add form

--- Bea Cukai/Controller/CalculatorViewController.swift
+++ Bea Cukai/Controller/CalculatorViewController.swift
@@ -10,29 +10,77 @@ import UIKit
 
 class CalculatorViewController: UITableViewController {
 
+    
+    var headerView: UIView = {
+        let header = UIView()
+        return header
+    }()
+    
+    let topTitle = ["FOB Barang ($)",
+                    "Kurs ($)",
+                    "Tarif Bea Masuk (%)",
+                    "Tarif PPN (%)",
+                    "Tarif PPN BM (%)",
+                    "NPWP - API",
+                    "Pembebasan FOB",
+                    "Asuransi ($)",
+                    "Freight ($)"]
+    
+    let textFieldPlaceholder = ["250",
+                                "13000",
+                                "10",
+                                "10",
+                                "10",
+                                "Pilih",
+                                "Pilih",
+                                "10",
+                                "10"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupContentView()
+    
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func setupContentView() {
+        
+        tableView.tableHeaderView = headerView
+
+        headerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        headerView.topAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
+        headerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        headerView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: 100)
+        
+
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return topTitle.count
     }
     
-    
-     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> CalculatorCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "CalculatorCell", for: indexPath)
-          
-        return cell as! CalculatorCell
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "CalculatorCell", for: indexPath) as! CalculatorCell
+        
+        cell.txtLabel.text = topTitle[indexPath.row]
+        cell.textField.placeholder = textFieldPlaceholder[indexPath.row]
+        
+        return cell
      }
     
     
