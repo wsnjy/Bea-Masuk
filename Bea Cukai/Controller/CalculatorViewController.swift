57c6f8d1ad816b6bbc07df6ed0f74bf000439f93
Wisnu Sanjaya <wisnu@sebangsa.com>
2017-09-13 08:42:41 +0700

    just add something

--- Bea Cukai/Controller/CalculatorViewController.swift
+++ Bea Cukai/Controller/CalculatorViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CalculatorViewController: UITableViewController {
 
+    let cellName = "CalculatorCell"
     
     var headerView: UIView = {
         let header = UIView()
@@ -35,10 +36,16 @@ class CalculatorViewController: UITableViewController {
                                 "Pilih",
                                 "10",
                                 "10"]
-
+    
+    func saveAction() {
+        print("Save action")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let btn: UIBarButtonItem = UIBarButtonItem.init(title: "Save", style: .plain, target: self, action: #selector(self.saveAction))
+        navigationItem.rightBarButtonItem = btn
         
         setupContentView()
     
@@ -54,7 +61,7 @@ class CalculatorViewController: UITableViewController {
         tableView.tableHeaderView = headerView
 
         headerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        headerView.topAnchor.constraint(equalTo: headerView.bottomAnchor).isActive = true
+        headerView.topAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         headerView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         headerView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         headerView.heightAnchor.constraint(equalToConstant: 100)
@@ -62,10 +69,14 @@ class CalculatorViewController: UITableViewController {
 
     }
     
+}
+
+extension CalculatorViewController {
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return topTitle.count
     }
@@ -73,16 +84,16 @@ class CalculatorViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 70
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell = tableView.dequeueReusableCell(withIdentifier: "CalculatorCell", for: indexPath) as! CalculatorCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath) as! CalculatorCell
         
         cell.txtLabel.text = topTitle[indexPath.row]
         cell.textField.placeholder = textFieldPlaceholder[indexPath.row]
         
         return cell
-     }
-    
-    
+    }
     
 }
+
+
