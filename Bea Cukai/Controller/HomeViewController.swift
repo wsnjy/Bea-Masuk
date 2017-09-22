cd77e7ba2a4de9667c25da9048fda286fdf48a3b
Wisnu Sanjaya <wisnu@sebangsa.com>
2017-09-22 17:36:08 +0700

    add launch image

---
+++ Bea Cukai/Controller/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  Bea Cukai
+//
+//  Created by Sanjaya Wisnu on 9/21/17.
+//  Copyright © 2017 Sanjaya Wisnu. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var stackViewRumus: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
